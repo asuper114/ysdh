94c1c2f429c3bd6ef4f1475b574c475693cba189
zhangyanchao <zhangyanchao@ichengzi.com>
2013-05-23 14:56:33 +0800

    init

---
+++ caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'ysdh',
+		'username' => 'root',
+		'password' => '123456',
+		'tablepre' => 'ysdh_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ phpsso_server/caches/caches_admin/caches_data/applist.cache.php
@@ -0,0 +1,16 @@
+<?php
+return array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'phpcms_v9',
+    'name' => 'phpcms v9',
+    'url' => 'http://ysdh.mau.edu.cn/',
+    'authkey' => 'TGfuNL1TXEmvt7dExPR4K4NKQkGcXR1I',
+    'ip' => '',
+    'apifilename' => 'api.php?op=phpsso',
+    'charset' => 'utf-8',
+    'synlogin' => '1',
+  ),
+);
+?>
